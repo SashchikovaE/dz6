9b95e5ad72b7e2f5075af656d5982815019a958f
Admin <Admin@USER>
2023-10-20 00:00:40 +0300

    Добавьте файлы проекта.

---
+++ dz6/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz6
+{
+    public class MobileOperators
+    {
+        enum RateMts
+        {
+            simply,
+            more,
+            super
+        }
+        enum RateTele2
+        {
+            lala,
+            fff
+        }
+        enum RateBeeline
+        {
+            kaka,
+            yaya
+        }
+        public string name { get; set; }
+    }
+    public class Subscriber : MobileOperators
+    {
+        public string namee;
+        public long numberOfTelephone;
+        public string mobileOperator;
+        public Subscriber(string namee, long numberOfTelephone, string mobileOperator)
+        {
+            this.namee = namee;
+            this.numberOfTelephone = numberOfTelephone;
+            this.mobileOperator = mobileOperator;
+        }
+        public Subscriber() { }
+        static Subscriber RegisterUser()
+        {
+            Console.WriteLine("введите имя пользователя");
+            string namee = Console.ReadLine();
+            Console.WriteLine("введите номер телефон");
+            long numberOfTelephone = long.Parse(Console.ReadLine());
+            Console.WriteLine("введите мобильного оператора, которым вы хотите пользоваться");
+            string mobileOperatorr = Console.ReadLine();
+            Subscriber subscriber = new Subscriber(namee, numberOfTelephone, mobileOperatorr);
+            Console.WriteLine("пользователь успешно зарегистрирован!");
+            return subscriber;
+        }
+        public double Balance { get; set; }
+
+        public void ToUpBalance(double amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine("некорректная сумма пополнения!");
+                return;
+            }
+            Console.WriteLine($"пополнение баланса на {amount} рублей. текущий баланс: {Balance} рублей");
+            Balance += amount;
+            Console.WriteLine($"баланс успешно пополнен. новый баланс: {Balance} рублей");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Subscriber subscriber = new Subscriber();
+            Console.WriteLine("введите сумму пополнения счёта");
+            double a = double.Parse(Console.ReadLine());
+            subscriber.ToUpBalance(a);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ tumakov7/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace tumakov7
+{
+    enum Type
+    {
+        Current,
+        Savings
+    }
+    class Bank
+    {
+        private long number;
+        private double balance;
+        private Type type;
+        public void SetNumber(long number)
+        {
+            this.number = number;
+        }
+        public long GetNumber()
+        {
+            return number;
+        }
+        public void SetBalance(double balance)
+        {
+            this.balance = balance;
+        }
+        public double GetBalance()
+        {
+            return balance;
+        }
+        public void SetType(Type type)
+        {
+            this.type = type;
+        }
+        public Type GetType(Type type)
+        {
+            return type;
+        }
+    }
+
+    class Program
+    {
+        static long PrintArray(int[] numbers)
+        {
+            foreach (int i in numbers)
+            {
+                Console.Write("{0}", i);
+            }
+            return 0;
+        }
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("упражнение 7.1");
+            Bank bank = new Bank();
+            bank.SetNumber(1234567890123456);
+            bank.SetBalance(45376);
+            bank.SetType(Type.Savings);
+            Console.WriteLine("номер счета: " + bank.GetNumber());
+            Console.WriteLine("баланс: " + bank.GetBalance());
+            Console.WriteLine("тип банковского счета: " + bank.GetType());
+
+            Console.WriteLine("упражнение 7.2");
+            Random random = new Random();
+            int[] numbers = new int[16];
+                for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = random.Next(0, 9);
+            }
+            bank.SetNumber(PrintArray(numbers));
+
+            Console.WriteLine("упражнение 7.3");
+            Console.WriteLine("введите баланс");
+            Console.WriteLine("введите сумму");
+            double Balance = double.Parse(Console.ReadLine());
+            double amount = double.Parse(Console.ReadLine());
+            if (amount > 0 && Balance >= amount)
+            {
+                Balance -= amount;
+                Console.WriteLine($"Списание {amount} успешно выполнено.\nНовый баланс: {Balance}");
+            }
+            else if (amount <= 0)
+            {
+                Console.WriteLine("Сумма для списания должна быть больше нуля.");
+            }
+            else
+            {
+                Console.WriteLine("Недостаточно средств на счете.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
